fd0a0eae62bfe23eeaad14e6a29fb0c57b364bff
Ashutosh_Kumar <kumarashutosh5689@gmail.com>
2025-03-19 09:49:07 +0530

    again back to development mode in dev branch

--- frontend/context/SocketContext.jsx
+++ frontend/context/SocketContext.jsx
@@ -16,20 +16,20 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     //for development
-    // const socket = io("http://localhost:5000", {
-    //   query: {
-    //     userId: user?._id,
-    //   },
-    // });
-
-    // for deployment
-    if (!user?._id) return;
-
-    const socket = io("/", {
+    const socket = io("http://localhost:5000", {
       query: {
         userId: user?._id,
       },
     });
+
+    // for deployment
+    if (!user?._id) return;
+
+    // const socket = io("/", {
+    //   query: {
+    //     userId: user?._id,
+    //   },
+    // });
     setSocket(socket);
     socket.on("getOnlineUsers", (users) => {
       setOnlineUsers(users);
